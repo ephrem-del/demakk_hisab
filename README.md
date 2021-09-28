46e29898722b94099c45619f7c4863cf9e264e17
Ephrem Mekuria <ephremmekuria@gmail.com>
2021-09-28 19:59:45 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# demakk_hisab
+ An app for keeping track of cash flow and orders for small business's
---
+++ android/app/src/main/kotlin/com/example/demakk_hisab/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demakk_hisab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
